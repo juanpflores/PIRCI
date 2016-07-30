bc593359ddb0358e23f562d83fcde2132fb97996
majofc <majofc1@gmail.com>
2016-07-30 03:57:09 -0500

    Modelo Actividad y carpetas de template

---
+++ Actividad/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+
+class Actividad(models.Model):
+    organismo = models.ForeignKey('auth.User')
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    #imagen ..... https://docs.djangoproject.com/es/1.9/ref/models/fields/
+    documentacion = models.TextField()
+    recompensa=models.DecimalField(..., max_digits=5, decimal_places=0)  
+    boton=models.BooleanField()
+
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.titulo

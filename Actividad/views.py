7eda238b6579dbd267f03284f3b91632c0533cc2
majofc <majofc1@gmail.com>
2016-07-30 16:49:41 -0500

    url a main-screen del dashboard

--- Actividad/views.py
+++ Actividad/views.py
@@ -8,12 +8,16 @@ def Landing(request):
     return render(request, 'landing/index.html', {})
 def Register(request):
 	return render(request, 'landing/register.html', {})
+
 def Welcome(request):
 	c=request.POST.get("myCurp")
 	p=request.POST.get("myPass")
 	e=request.POST.get("myEmail")
 	t=request.POST.get("myTitle")
 	Usuario.objects.create(curp=c, password=p, monedas='0', correo=e,title=t)
-	return render(request,'landing/registerUser.html')
-	
+	#me = User.objects.get(username='ola')
+	return render(request, 'dashboard/main-Screen.html')
 
+def Dashboard (request):
+	#usuario=request.POST.get("myTitle")
+	return render(request,'dashboard/main-Screen.html')

66ff9cf2871b96a6e3015e0010fee28ab4c99305
majofc <majofc1@gmail.com>
2016-07-30 09:11:43 -0500

    Added model Ciudadano to Actividad and Actividad.admin

--- Actividad/admin.py
+++ Actividad/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Actividad
+from .models import Ciudadano
 
 # Register your models here.
+admin.site.register(Actividad)
+admin.site.register(Ciudadano)
--- Actividad/models.py
+++ Actividad/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Actividad(models.Model):
-    organismo = models.ForeignKey('auth.User')
+    organismo = models.CharField(max_length=30)
     titulo = models.CharField(max_length=200)
     descripcion = models.TextField()
     #imagen ..... https://docs.djangoproject.com/es/1.9/ref/models/fields/
@@ -17,3 +17,17 @@ class Actividad(models.Model):
 
     def __str__(self):
         return self.titulo
+
+class Ciudadano(models.Model):
+    curp = models.CharField(max_length=20)
+    userName = models.CharField(max_length=30)
+    password = models.CharField(max_length=20)
+    monedas=models.IntegerField()
+
+
+   
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.title

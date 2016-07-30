f89080c9f30b912cd405edf5138b36e37e6f03b4
majofc <majofc1@gmail.com>
2016-07-30 07:36:32 -0500

    prepared register

--- Actividad/urls.py
+++ Actividad/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.Landing, name='Landing'),
+    url(r'register$', views.Register, name='Register'),
 ]
--- Actividad/views.py
+++ Actividad/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def Landing(request):
     return render(request, 'landing/index.html', {})
+def Register(request):
+	return render(request, 'landing/register.html', {})

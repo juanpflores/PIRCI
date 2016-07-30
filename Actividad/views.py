d101f5018bff2437c46f074df78f5b90e5eed9f3
Juan Pablo <juanpflores94@gmail.com>
2016-07-30 05:59:30 -0500

    works

--- Actividad/views.py
+++ Actividad/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def Landing(request):
-    return render(request, '../../templates/landing/index.html', {})
+    return render(request, 'landing/index.html', {})
